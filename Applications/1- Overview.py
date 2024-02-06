82e3296dc6e0ff36ddb16331b182610b3fb79db2
WezDean <mcoddish@gmail.com>
2024-02-06 10:58:30 +0800

    Commiting

---
+++ Applications/1- Overview.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+import pandas as pd 
+
+st.title('Gun Violence in America')
+st.markdown('This application showcases in depth analysis of overall gun violence in America')
+
--- Pages/1- Overview.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st 
-import pandas as pd 
-
-st.title('Gun Violence in America')
-st.markdown('Deaths recorded due to Guns in the USA')
