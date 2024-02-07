61ae56482dd754a33f9e38937d40e5dba70bafcb
WezDean <mcoddish@gmail.com>
2024-02-07 09:28:57 +0800

    please

--- Applications/1- Overview.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st 
-import pandas as pd 
-
-st.title('Gun Violence in America')
-st.markdown('This application showcases in depth analysis of overall gun violence in America')
-st.image("GunOverview.jpg")
